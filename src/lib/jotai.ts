a6ab4b91125aadc4fabcf767599ea7795405f382
Tao Xin <tao@vanjs.org>
2023-08-10 18:04:47 -0700

    feat: simplified jotai intergration logic

--- src/lib/jotai.ts
+++ src/lib/jotai.ts
@@ -12,10 +12,6 @@ export function vanjsJotaiFactory(): UseAtom {
   return <Value, Args extends unknown[], Result>(atom: WritableAtom<Value, Args, Result> | Atom<Value>) => {
     const atomState = van.state(store.get(atom))
     return new Proxy(atomState, {
-      get(state, prop) {
-        const r = Reflect.get(state, prop)
-        return prop === 'val' ? store.get(atom) : r
-      },
       set(state, prop, newValue: Value) {
         const ret = Reflect.set(state, prop, newValue)
         if (prop === 'val' && 'write' in atom && newValue !== store.get(atom)) {
